f1545ef8dbe59d1b609aa78be3b1d40a554cab59
dominikkiepura1 <dominik.kiepura1@microsoft.wsei.edu.pl>
2024-11-03 20:15:15 +0100

    Zadanie bez działającego edita

---
+++ Film3/Models/Film.cs
@@ -0,0 +1,11 @@
+﻿namespace Film3.Models
+{
+    public class Film
+    {
+        public int ID { get; set; }
+        public int Id { get; internal set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+    }
+}
